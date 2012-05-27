4f1870defc1c278682118bbe3599ba7212449ed3
Takayuki Otake <k.wisiiy@gmail.com>
2012-05-28 00:29:34 +0900

    実行オプション作成   -b BRANCH 作成リリースの元ブランチ。デフォルトはmaster   -h        ヘルプを表示   -l        `git clone`せずに../SetucoCMS/ディレクトリを元ディレクトリとして利用   -v        このスクリプトのバージョンを表示

--- make_release.sh
+++ make_release.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# SetucoCMSのリリースを作成するスクリプト
-# 引数に作成するバージョンを指定するとリリース用の圧縮ファイルとgitのタグが生成される
-# 
-# 例)
-# $ sh make_release.sh 1.3.0
-#
-# スクリプトを実行した時点でのdensetubu/SetucoCMSのmasterから取ってくるので柔軟性に欠けるかもしれない
+VERSION='0.1'
+
+CMDNAME=$(basename $0)
+
+help() {
+    echo
+    echo 'SetucoCMSのリリースを作成するスクリプト'
+    echo "書式 : $CMDNAME [-b BRANCH] [-l] [-h] VERSION"
+    echo
+    echo '    -b BRANCH    作成リリースの元ブランチ。デフォルトは master'
+    echo '    -h           ヘルプを表示'
+    echo '    -l           `git clone`せずに../SetucoCMS/ディレクトリを元ディレクトリとして利用。'
+    echo '    -v           このスクリプトのバージョンを表示'
+    echo
+}
 
 is_numeric() {
     expr "$1" + 1 > /dev/null 2>&1
@@ -17,41 +25,90 @@ is_numeric() {
     fi
 }
 
+#オプション処理
+FLG_CLONE=1
+while getopts hlvb: OPT
+do
+    case $OPT in
+        "b" ) BRANCH="$OPTARG" ;;
+        "h" ) help ; exit ;;
+        "l" ) FLG_CLONE=0 ;;
+        "v" ) echo "バージョン: " $VERSION; exit ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+#リリース元ブランチ
+if [ -n $BRANCH ]
+then
+    echo -n ''
+else
+    BRANCH='master'
+fi
+
+
 if [ -z $1 ]
 then
-    echo '引数にリリースするバージョンを指定してください'
-    echo '実行例) $' $0 '1.2.3'
+    help
+
+    #TODO: 標準エラー出力
+    echo 'error : 引数にリリースするバージョンを指定してください'
+    echo '例) $' $0 '1.2.3'
     exit 1
 fi
 
-v_major=$(echo $1 | cut -d '.' -f 1)
-v_minor=$(echo $1 | cut -d '.' -f 2)
-v_patch=$(echo $1 | cut -d '.' -f 3)
-
-is_numeric $v_major; res_v_major=$?
-is_numeric $v_minor; res_v_minor=$?
-is_numeric $v_patch; res_v_patch=$?
-
+#バージョン指定のチェック
+v_major=$(echo $1 | cut -d '.' -f 1); is_numeric $v_major; res_v_major=$?
+v_minor=$(echo $1 | cut -d '.' -f 2); is_numeric $v_minor; res_v_minor=$?
+v_patch=$(echo $1 | cut -d '.' -f 3); is_numeric $v_patch; res_v_patch=$?
 if [ $res_v_major -gt 0 -o $res_v_minor -gt 0 -o $res_v_patch -gt 0 ]
 then
-    echo '書式が不正です'
+    help
+
+    #TODO: 標準エラー出力
+    echo 'error : 書式が不正です'
     echo '引数にリリースするバージョンを指定してください'
-    echo '実行例) $' $0 '1.2.3'
-    exit 1
+    echo '例) $' $0 '1.2.3'
+    exit 2
 fi
 
-if [ -f 'SetucoCMS' -o -d 'SetucoCMS' ]
-then
-    echo 'ファイルかディレクトリが既に存在します'
-    exit
-fi
 
+## リリース作成 ここから
 echo 'バージョン' $v_major.$v_minor.$v_patch 'のリリースを作成します'
 echo
 
-git clone http://github.com/densetubu/SetucoCMS.git
+if [ $FLG_CLONE -gt 0 ]
+then
+    echo 'リモートのmasterブランチからリリースを作成します。'
+    #ディレクトリチェック
+    if [ -f 'SetucoCMS' -o -d 'SetucoCMS' ]
+    then
+        #TODO: 標準エラー出力
+        echo 'ファイルかディレクトリが既に存在します'
+        exit 3
+    fi
 
-cd SetucoCMS
+    git clone http://github.com/densetubu/SetucoCMS.git
+    cd SetucoCMS
+else
+    if [ -d '../SetucoCMS' ]
+    then
+        cd ../SetucoCMS
+        echo `pwd` ’ディレクトリを元にリリースを作成します’
+    else
+        cd ../
+        echo "`pwd`/SetucoCMS/ ディレクトリが存在しません。"
+        exit 4
+    fi
+fi
+
+git checkout $BRANCH
+if [ $? -gt 0 ]
+then
+    #TODO: 標準エラー出力
+    echo '存在しないブランチです : ' $BRANCH
+    exit 5
+fi
 git checkout -b $v_major.$v_minor
 #git push origin $v_major.$v_minor
 git tag v$v_major.$v_minor.$v_patch
