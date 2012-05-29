34d003aa21eaff7cdccc27918bf4a6ed0e8f71d6
suzuki_mar <supasu145@gmail.com>
2012-05-29 18:30:05 +0900

    ディレクトリ定義をするためにConstDirPathクラスを作成した
    ディレクトリの定義が分散しないようにこのファイルにまとめる

---
+++ script/php/ConstDirPath.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * toolsのディレクトリ構造を定義しています
+ *
+ *
+ *
+ * @copyright   Copyright (c) 2010 SetucoCMS Project.
+ * @author suzuki-mar
+ */
+class ConstDirPath
+{
+    public static function ROOT_PATH()
+    {
+        return ROOT_PATH;
+    }
+
+    public static function SCRIPT_PATH()
+    {
+        return self::ROOT_PATH() . '/script/php';
+    }
+
+    public static function TEST_PATH()
+    {
+        return self::ROOT_PATH() . '/tests/php';
+    }
+
+    public static function FIXTURE_PATH()
+    {
+        return self::TESt_PATH() . "/fixture";
+    }
+
+    public static function FIXTURE_FILE_PATH()
+    {
+        return self::FIXTURE_PATH() . '/file';
+    }
+}
+
+
+
